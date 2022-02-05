3b3c1e596c3b3be108030345837044cf3b26885b
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-02-05 11:13:16 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/courses", (req, res) => {
+    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.listen(3333);
