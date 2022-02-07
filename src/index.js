4c4bedc15c973c6df7219805ad7c1f49491eb00a
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-02-07 18:28:51 -0300

    Criação de conta, validação de conta e adicionando middlewares

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 
-app.get("/courses", (req, res) => {
-    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+app.use(express.json());
+
+const customers = [];
+
+function verifyIfExistsAccoutCPF(req, res, next) {
+    const { cpf } = req.headers;
+
+    const customer = customers.find((customers) => customers.cpf === cpf);
+
+    if(!customer){
+        return res.status(400).json({error: 'Customer not found'})
+    }
+
+    req.customer = customer;
+
+    return next();
+}
+
+app.post("/account", (req, res) => {
+    const { name, cpf } = req.body;
+
+    const costumersAlreadyExist = customers.some((customers) => customers.cpf ===  cpf);
+
+    if(costumersAlreadyExist){
+        return res.status(400).json({error: "Costumers already exists!"})
+    }
+
+    customers.push({ 
+        cpf,
+        name,
+        id:  uuidv4(),
+        statement: [],
+    });
+
+    return res.status(201).send();
+});
+
+app.get("/statement", verifyIfExistsAccoutCPF, (req, res) => {
+    const { customer } = req;
+    return res.json(customers.statement);
 });
 
 app.listen(3333);
